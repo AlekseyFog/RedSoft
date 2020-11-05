829d984154ddfa71550f8bad1f99be9a019df60b
Aleksey <AlekseyFog@github.com>
2020-11-05 20:25:06 +0300

    interface improves

--- config/config.base.js
+++ config/config.base.js
@@ -2,4 +2,5 @@ module.exports = {
     outputDir: 'docs',
     runtimeCompiler: false,
     productionSourceMap: false,
+    publicPath: process.env.NODE_ENV === 'production' ? '/RedSoft/' : '/'
 };
